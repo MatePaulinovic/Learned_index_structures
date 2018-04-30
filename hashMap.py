1d4132ffb01842306e92c4702922f5d5829b3be0
MatePaulinovic <mate.paulinovic123@gmail.com>
2018-04-30 14:19:20 +0200

    Dataprep optimization

---
+++ hashMap.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+@author: matep
+"""
+
+class HashMap:
+    
+    hash_values = {
+                'A' : 0,
+                'C' : 1,
+                'G' : 2,
+                'T' : 3
+        }
+    
+    def __init__(self, size=100):
+        self.table = [[] for bucket in range(size)]
+        self.size = size
+        self.collisions = 0
+        self.filled_slots = 0
+        
+    
+        
+    
+    def insert(self, key, value):
+        key_index = self.hash_kmer(key) % self.size
+        bucket = self.table[key_index]
+        
+        bucket = list(filter(lambda t: t[0] != key, bucket))
+        bucket.append((key, value))
+        
+
+    def get(self, key):
+        key_index = self.hash_kmer(key) % self.size
+        bucket = self.table[key_index]
+        
+        for k,v in bucker:
+            if k == key:
+                return value
+            
+        return None
+        
+    def hash_kmer(self, key):
+        k = len(kmer)
+        hash_value = 0
+        
+        for i in range(0, k):
+            hash_value += pow(4, k - i - 1) * self.hash_value(kmer[i]) 
+        
+        return hash_value    
+        
+        
+        
